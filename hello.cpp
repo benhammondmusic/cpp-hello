a105a5c8a3eb026ec410d6a75a5f3f5cb8e0d8f3
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-04-02 14:04:40 -0600

    testing out loops, functions, if statements

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,31 @@
+// https://medium.com/@olehc/lets-write-a-simple-sine-wave-generator-with-c-and-juce-c8ab42d1f54f
+
 /* hello.cpp */
 #include <iostream>
 using namespace std;
+
+
+
+string doubleName(string name){
+    return name + name;
+}
+
+
+
 int main()
 {
-    cout << "Please enter your name: ";
-string name;
+    string name;
+    
+    while (name != "b")
+    {
+        cout << "Please enter your name: ";
+    
     cin >> name;
-    cout << "Hello, " << name;
+        cout << "your name must be b to exit\n";
+    }
+    cout << "Hello, " << doubleName(name);
+    
     cout << "\n";
+
 return 0;
 }
